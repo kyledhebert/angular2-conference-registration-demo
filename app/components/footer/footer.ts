8be92617ac69866f50fa2d3cbf18054f305673cd
kylehebert <kyle.hebert@gmail.com>
2016-11-16 19:54:16 -0600

    Moves app directory to client directory

--- app/components/footer/footer.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-    selector: 'registration-footer',
-    templateUrl: 'app/components/footer/footer.html'
-})
-
-export class FooterComponent {}
--- app/components/navbar/navbar.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-    selector: 'registration-navbar',
-    templateUrl: 'app/components/navbar/navbar.html'
-})
-
-export class NavbarComponent {}
